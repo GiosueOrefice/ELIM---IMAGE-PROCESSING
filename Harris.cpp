9736634a0a69fbf62f107d67300d8b872b4a29ae
Giosue <79579641+GiosueOrefice@users.noreply.github.com>
2022-03-08 09:43:38 +0100

    Update Harris.cpp

--- Harris.cpp
+++ Harris.cpp
@@ -7,24 +7,27 @@ using namespace cv;
 
 void harrisCornering(Mat src, Mat& dst, float k, int thresholdValue) {
 
-    //Calcolo le derivate parziali rispetto a x e y, dx², dy² e dx*dy
+    //1. Calcolare le derivate parziali rispetto ad x e y (Dx, Dy)
     Mat dx, dx_2;
     Sobel(src, dx, CV_32FC1, 1, 0, 3, BORDER_DEFAULT);
 
     Mat dy, dy_2;
     Sobel(src, dy, CV_32FC1, 0, 1, 3, BORDER_DEFAULT);
 
+    //2. Calcolare Dx2, Dy2 e Dx*Dy
     pow(dx, 2, dx_2);//C00
     pow(dy, 2, dy_2);//C11
 
     Mat dxdy;
     multiply(dx, dy, dxdy);//C01, C10
 
-    //Faccio un po di smoothing
+    //3. Applicare un filtro Gaussiano a Dx2, Dy2 e Dx* Dy
+    //4. Si ottengono C00, C11, C01 (e quindi anche C10)
     GaussianBlur(dx_2, dx_2, Size(5,5), 2.0, 0.0, BORDER_DEFAULT);
     GaussianBlur(dy_2, dy_2, Size(5, 5), 0.0, 2.0, BORDER_DEFAULT);
     GaussianBlur(dxdy, dxdy, Size(5, 5), 2.0, 2.0, BORDER_DEFAULT);
 
+    //5. Calcolare l'indice R
     Mat dx2_dy2;
     multiply(dx_2, dy_2, dx2_dy2);
     Mat dxdy2;
@@ -34,9 +37,11 @@ void harrisCornering(Mat src, Mat& dst, float k, int thresholdValue) {
     Mat traccia_2;
     pow((dx_2 + dy_2), 2, traccia_2); //Traccia = C00+C11
 
-    Mat R = detrm - k * traccia_2; //R = determinante + k*traccia²
-    normalize(R, R, 0, 255, NORM_MINMAX, CV_32FC1);
+    Mat R = detrm - k * traccia_2; //R = determinante + k*tracciaÂ²
 
+    // 6. Normalizzare l'indice R in [0, 255]
+    normalize(R, R, 0, 255, NORM_MINMAX, CV_32FC1);
+    //7. Sogliare R
     for (int i = 0; i < src.rows; i++)
         for (int j = 0; j < src.cols; j++)
             if ((int)R.at<float>(i, j) > thresholdValue)
